1649ba7353b64267b455a50a7a3230c598acb055
thomasjm <tom@codedown.io>
2023-03-23 18:13:47 -0700

    Get rid of unused stuff

--- nix/static_libsodium.nix
+++
@@ -1,9 +0,0 @@
-{ pkgsCross }:
-
-(pkgsCross.musl64.libsodium.override { }).overrideAttrs (oldAttrs: {
-  NIX_CFLAGS_COMPILE = "-static";
-  configureFlags = (oldAttrs.configureFlags or []) ++ [
-    "--enable-static"
-    "--disable-shared"
-  ];
-})
